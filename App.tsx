ec1eae6c41c9d72414f57de233f2bda36252c505
Rosa Blanca <rosablanca.pg@gmail.com>
2025-09-15 00:26:52 -0300

    build:  added dependencies

--- App.tsx
+++ App.tsx
@@ -1,45 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// Import polyfills first to fix AWS SDK compatibility issues
+import './src/polyfills';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { store } from './src/application/store';
+import { AppNavigator } from './src/presentation/navigation/AppNavigator';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" />
+        <AppNavigator />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+
+import 'react-native-gesture-handler';
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+import { configureAmplify } from './src/shared/config/aws-config';
+configureAmplify();
 
 AppRegistry.registerComponent(appName, () => App);
